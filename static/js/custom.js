01ab05668fb1e97ad4d2266eff2a76d7402bb6b9
Charles Chin <eallions@gmail.com>
2022-03-13 12:40:49 +0800

    style: add back to top button

--- static/js/custom.js
+++ static/js/custom.js
@@ -429,8 +429,8 @@ if (jQuery(".comment_stars a").click((function () {
       }))
   }
   t(window).scroll((function () {
-      t(window).scrollTop() > 200 ? t("#back-to-top").addClass("active") : t("#back-to-top").removeClass("active")
-  })), t("#back-to-top").click((function () {
+      t(window).scrollTop() > 200 ? t("#back_to_top").addClass("active") : t("#back_to_top").removeClass("active")
+  })), t("#back_to_top").click((function () {
       return t("body,html").animate({
           scrollTop: 0
       }, 800), !1
@@ -613,4 +613,80 @@ $(document).ready(function () {
           $(this).parents('#index-talk').remove();
       });
   }
+});
+
+/**
+ *  Plugin Name: jQuery toTop for smoothly Scroll back to Top
+ *  Plugin URL: https://github.com/mmkjony/jQuery.toTop
+ *  Version: 1.1
+ *  Author: MMK Jony
+ *  Author URL: https://github.com/mmkjony
+ *  License: Licensed under MIT
+ **/
+
+(function( $ ){
+  'use strict';
+  
+  $.fn.toTop = function(opt){
+      
+      //variables
+      var elem = this;
+      var win = $(window);
+      var doc = $('html, body');
+      
+      //Extended Options
+      var options = $.extend({
+          autohide: true,
+          offset: 420,
+          speed: 500,
+          position: true,
+          right: 15,
+          bottom: 30
+      }, opt);
+      
+      elem.css({
+          'cursor': 'pointer'
+      });
+      
+      if(options.autohide){
+          elem.css('display', 'none');
+      }
+      
+      if(options.position){
+          elem.css({
+              'position': 'fixed',
+              'right': options.right,
+              'bottom': options.bottom,
+          });
+      }
+      
+      elem.click(function(){
+          doc.animate({scrollTop: 0}, options.speed);
+      });
+      
+      win.scroll(function(){
+          var scrolling = win.scrollTop();
+          
+          if(options.autohide){
+              if(scrolling > options.offset){
+                  elem.fadeIn(options.speed);
+              }
+              else elem.fadeOut(options.speed);
+          }
+          
+      });
+      
+  };
+  
+}( jQuery ));
+
+// 回到顶部 custom
+$('.to-top').toTop({
+  //options with default values
+  autohide: true,
+  offset: 420,
+  speed: 500,
+  position: true,
+  right: 15,
+  bottom: 30
 });
