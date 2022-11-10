20059fbec5d6be8a3de80f52135790100514573f
Charles Chin <eallions@gmail.com>
2022-11-11 01:23:12 +0800

    refactor: announcement memos

--- static/js/custom.js
+++ static/js/custom.js
@@ -643,15 +643,15 @@ if (jQuery(".comment_stars a").click((function () {
 // 首页调用嘀咕 JSON 版
 $(document).ready(function () {
   if ($("#index-talk").length > 0) {
-    jsonUrl = "https://eallion.com/talks.json"
+    jsonUrl = "https://eallion.com/memos.json"
     $.getJSON(jsonUrl + "?t=" + Date.parse(new Date()), function (res) {
       var bberCount = res.count;
       var talksHtml = ''
       $.each(res.data, function (i, item) {
-        d = new Date(item.date)
+        d = new Date(item.createdTs * 1000);
         date = d.getFullYear() + '/' + (d.getMonth() + 1) + '/' + d.getDate() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds()
         dataTime = '<span class="datatime">' + date + '</span>'
-        talksHtml += '<li class="item item-' + (i + 1) + '">' + dataTime + '： <a href="https://eallion.com/talk/" target="_blank" rel="noopener noreferrer">' + urlToLink(item.content) + '</a></li>'
+        talksHtml += '<li class="item item-' + (i + 1) + '">' + dataTime + '： <a href="https://eallion.com/memos/" target="_blank" rel="noopener noreferrer">' + urlToLink(item.content) + '</a></li>'
       });
       $('#index-talk').append('<ul class="talk-list">' + talksHtml + '</ul>')
       Lately({
@@ -660,14 +660,11 @@ $(document).ready(function () {
     });
 
     function urlToLink(str) {
-      var re = /\bhttps?:\/\/(?!\S+(?:jpe?g|png|bmp|gif|webp|jfif|gif))\S+/g;
-      var re_forpic = /\bhttps?:[^:<>"]*\/([^:<>"]*)(\.(jpe?g)|(png)|(bmp)|(jfif)|(webp))/g;
-      str = str.replace(re, function (website) {
-        return '<span class="iconify-inline" data-icon="bx:link-external"></span>';
-      });
-      str = str.replace(re_forpic, function (imgurl) {
-        return '<span class="iconify-inline" data-icon="bi:image-fill"></span>';
-      });
+     str = str .replace(/```([\s\S]*?)```[\s]*/g,' <code>$1</code> ')                                                                //全局匹配代码块
+            .replace(/`([\s\S ]*?)`[\s]*/g,' <code>$1</code> ')                                                                         //全局匹配内联代码块
+            .replace(/\!\[[\s\S]*?\]\([\s\S]*?\)/g,'🌅')                                                                                       //全局匹配图片
+            .replace(/\[[\s\S]*?\]\([\s\S]*?\)/g,'🔗')                                                                                         //全局匹配连接
+            .replace(/\bhttps?:\/\/(?!\S+(?:jpe?g|png|bmp|gif|webp|jfif|gif))\S+/g,'🔗')                                 //全局匹配纯文本连接
       return str;
     }
 
